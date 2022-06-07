cc0673b3adf40d0055dfd26d4d6102f2b4e74560
Mochammad Yusuf Habiburrahman <99495147+yusufhabibmochy@users.noreply.github.com>
2022-06-07 16:10:22 +0700

    Add files via upload

---
+++ assets/dropdown.js
@@ -0,0 +1,19 @@
+function myDropdown() {
+    document.getElementById("showDropdown").classList.toggle("show");
+  }
+  window.onclick = function(event) 
+  {
+    if (!event.target.matches('.button')) 
+    {
+      var dropdowns = document.getElementsByClassName("contentDropdown");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) 
+      {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) 
+        {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
